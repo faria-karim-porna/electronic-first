35ed3cb49e02adcc7552accb87148cd4fad89a6e
faria-karim-porna <faria.porna.08@gmail.com>
2023-08-20 13:37:23 +0000

    Partially Fixed Search Bar

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import "./style.scss";
 
 const childMenu = document.getElementsByClassName("child-menu")[0];
+const searchBoxArea = document.getElementsByClassName("search-area-box")[0];
+const searchBarArea = document.getElementsByClassName("search-bar-area")[0];
 
 window.openChildMenu = () => {
   childMenu.style.display = "block";
+};
+
+window.search = () => {
+  searchBoxArea.style.display = "none";
+  searchBarArea.style.display = "flex";
+};
+
+window.closeSearch = () => {
+  searchBoxArea.style.display = "flex";
+  searchBarArea.style.display = "none";
 };
 
 function closeModal() {
