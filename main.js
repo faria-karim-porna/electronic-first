eb269a2740994611d0b3af29111db3b23055b873
faria-karim-porna <faria.porna.08@gmail.com>
2023-08-20 09:59:57 +0000

    Updated Modal

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 import "./style.scss";
 
-// Get references to the modal and the button that opens it
 const modal = document.getElementById("myModal");
-const openModalBtn = document.getElementById("openModalBtn");
-const closeModalBtn = modal.querySelector(".close");
 
-// Function to open the modal
-function openModal() {
+window.openModal = () => {
   modal.style.display = "block";
-}
+};
 
-// Function to close the modal
 function closeModal() {
   modal.style.display = "none";
 }
 
-// Event listeners
-openModalBtn.addEventListener("click", openModal);
-closeModalBtn.addEventListener("click", closeModal);
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
     closeModal();
