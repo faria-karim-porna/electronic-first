704615cf29bed5c5a5e91f6311920396128d3cdb
faria-karim-porna <faria.porna.08@gmail.com>
2023-08-20 14:43:04 +0000

    Fixed Search Area

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.scss";
 const childMenu = document.getElementsByClassName("child-menu")[0];
 const searchBoxArea = document.getElementsByClassName("search-area-box")[0];
 const searchBarArea = document.getElementsByClassName("search-bar-area")[0];
+const searchCloseIcon = document.getElementsByClassName("search-close-icon")[0];
 
 window.openChildMenu = () => {
   childMenu.style.display = "block";
@@ -11,11 +12,13 @@ window.openChildMenu = () => {
 window.search = () => {
   searchBoxArea.style.display = "none";
   searchBarArea.style.display = "flex";
+  searchCloseIcon.style.display = "block";
 };
 
 window.closeSearch = () => {
   searchBoxArea.style.display = "flex";
   searchBarArea.style.display = "none";
+  searchCloseIcon.style.display = "none";
 };
 
 function closeModal() {
